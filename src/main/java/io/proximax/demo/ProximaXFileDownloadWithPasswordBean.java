45d8c525b1b8c4e54d1680f503a8b15e5d615a22
Carlo Cayos <carlocayos@gmail.com>
2018-08-01 14:50:11 +1000

    Update xpx-java-sdk version and package name

--- src/main/java/io/proximax/demo/ProximaXFileDownloadWithPasswordBean.java
+++ src/main/java/io/proximax/demo/ProximaXFileDownloadWithPasswordBean.java
@@ -1,27 +1,17 @@
 package io.proximax.demo;
 
-import io.nem.xpx.facade.connection.RemotePeerConnection;
-import io.nem.xpx.facade.download.Download;
-import io.nem.xpx.facade.download.DownloadException;
-import io.nem.xpx.facade.download.DownloadParameter;
-import io.nem.xpx.facade.download.DownloadResult;
-import io.nem.xpx.facade.upload.Upload;
-import io.nem.xpx.facade.upload.UploadResult;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import io.proximax.xpx.facade.connection.RemotePeerConnection;
+import io.proximax.xpx.facade.download.Download;
+import io.proximax.xpx.facade.download.DownloadParameter;
+import io.proximax.xpx.facade.download.DownloadResult;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.faces.context.ResponseWriter;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.UploadedFile;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -30,44 +20,42 @@ import org.primefaces.model.UploadedFile;
  */
 
 /**
- *
  * @author Alvin
  */
 
 @ManagedBean(name = "fileDownloadWithPasswordBean")
 @SessionScoped
 public class ProximaXFileDownloadWithPasswordBean {
-    
+
 
     private String password;
     private String hash;
     private String node = "https://testnet.gateway.proximax.io/";
 
 
-    
     public void downloadFile() {
 
-            try {
-                Download download = new Download(new RemotePeerConnection(this.node));
-                DownloadResult result = download.downloadBinary(DownloadParameter.create().nemHash(this.getHash()).securedWithPasswordPrivacyStrategy(this.password).build());
+        try {
+            Download download = new Download(new RemotePeerConnection(this.node));
+            DownloadResult result = download.downloadBinary(DownloadParameter.create().nemHash(this.getHash()).securedWithPasswordPrivacyStrategy(this.password).build());
 
-                final FacesContext fc = FacesContext.getCurrentInstance();
-                final ExternalContext externalContext = fc.getExternalContext();
-                externalContext.responseReset();
-                externalContext.setResponseContentType(result.getDataMessage().type());
-                externalContext.setResponseHeader("Content-Disposition", "attachment;filename=" + result.getDataMessage().name());
+            final FacesContext fc = FacesContext.getCurrentInstance();
+            final ExternalContext externalContext = fc.getExternalContext();
+            externalContext.responseReset();
+            externalContext.setResponseContentType(result.getDataMessage().type());
+            externalContext.setResponseHeader("Content-Disposition", "attachment;filename=" + result.getDataMessage().name());
 
-                final HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
-                final ServletOutputStream out = response.getOutputStream();
+            final HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
+            final ServletOutputStream out = response.getOutputStream();
 
-                out.write(result.getData());
-                out.flush();
-                fc.responseComplete();
+            out.write(result.getData());
+            out.flush();
+            fc.responseComplete();
 
-        }catch(Exception e) {
+        } catch (Exception e) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Failed to download: Make sure that Hash/Password is correct and the NEM Transaction has been confirmed."));
         }
-       
+
     }
 
     /**
@@ -111,6 +99,6 @@ public class ProximaXFileDownloadWithPasswordBean {
     public void setNode(String node) {
         this.node = node;
     }
-    
-    
+
+
 }
